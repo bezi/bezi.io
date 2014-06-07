b284830cbd8d902947a6f076021d05f42a8e706c
Oscar Bezi <oscar@bezi.io>
2014-06-07 18:13:29 -0400

    Perfected ssh-keys.  Need to add the coloring to the output.

--- utils/init.sh
+++ utils/init.sh
@@ -9,60 +9,98 @@
 # download files and stick them in the right places, then source the ~/.bashrc
 # note that I didn't get a .bash_locations: this is a per-computer file
 echo "Downloading files. . .";
-cd ~/;
 
 URL_ROOT='https://raw.githubusercontent.com/bezi/bezi.github.io/master/utils';
 # .bashrc
-wget -nv -O ~/.bashrc $URL_ROOT/.bashrc;
+#wget -nv -O ~/.bashrc $URL_ROOT/.bashrc;
 # .bash_aliases
-wget -nv -O ~/.bash_aliases $URL_ROOT/.bash_aliases;
+#wget -nv -O ~/.bash_aliases $URL_ROOT/.bash_aliases;
 # .vimrc
-wget -nv -O ~/.vimrc $URL_ROOT/.vimrc;
+#wget -nv -O ~/.vimrc $URL_ROOT/.vimrc;
 # .ssh/config
-wget -nv -O ~/.ssh/config $URL_ROOT/.ssh/config;
+#wget -nv -O ~/.ssh/config $URL_ROOT/.ssh/config;
 
 echo "|-- Done!";
 
 shopt -s expand_aliases;
 source ~/.bashrc;
 
+# programs I use a lot. gimme is an alias to the installer for the OS
+#gimme vim;
+#gimme git;
+# gimme sshpass;
+#gimme xclip;
+#gimme atool;
+
 
 # git configuration
 echo "Configuring git. . .";
-gimme git;
-git config --global user.name "Oscar Bezi";
-git config --global user.email "oscar@bezi.io";
-git config --global color.ui true;
-git config --global push.default simple;
+#git config --global user.name "Oscar Bezi";
+#git config --global user.email "oscar@bezi.io";
+#git config --global color.ui true;
+#git config --global push.default simple;
 echo "|-- Done!";
 
 # generate SSH keys and propagate them to the various things in ~/.ssh/config
 echo "Distributing ssh keys. . .";
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa;
+yes y | ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa;
 
+
+# generate password files
 for i in `cat ~/.ssh/config | grep "^host " | awk '{print $2}'`; do 
-    echo "Adding keys to host $i. . .";
-    echo ssh-copy-id $i; 
+    read -s -p "Adding keys to host $i.  Password: " password;
+    echo; # new line
+    echo $password > $i.password
+    sshpass -f $i.password ssh-copy-id $i > /dev/null 2>&1;
+    
+    STATUS=$?;
+    if [ "$STATUS" == "5" ]; then
+        echo "Wrong password!";
+        while [ "$STATUS" == "5" ]; do
+            read -s -p "Password: " password;
+            echo; # new line
+            echo $password > $i.password
+            sshpass -f $i.password ssh-copy-id $i > /dev/null 2>&1;
+            STATUS=$?;
+            if [ "$STATUS" == "5" ]; then
+                echo "Wrong password!";
+                while [ "$STATUS" == "5" ]; do
+                    read -s -p "Password: " password;
+                    echo; # new line
+                    echo $password > $i.password
+                    sshpass -f $i.password ssh-copy-id $i > /dev/null 2>&1;
+                    STATUS=$?;
+                done;
+            elif [ "$STATUS" == "0" ]; then
+                echo "Success!";
+            else 
+                echo "FAILURE: STATUS $STATUS";
+            fi
+        done;
+    elif [ "$STATUS" == "0" ]; then
+        echo "Success!";
+    else 
+        echo "FAILURE: STATUS $STATUS";
+    fi
+    
 done
 
 # add ssh keys to github: cannae automate :(
-gimme xclip;
 # from github; copies the key to my clipboard so I can go paste it into ffox
-xclip -sel clip < ~/.ssh/id_rsa.pub
+#xclip -sel clip < ~/.ssh/id_rsa.pub
 
-echo "  SSH key copied to clipboard.  Opening firefox to add to github. . ." &;
-gimme firefox;
-firefox https://github.com/settings/ssh;
-echo "|-- Done!";
+#echo "|-- SSH key copied to clipboard.  Opening firefox to add to github. . ." ;
+#gimme firefox;
+#firefox https://github.com/settings/ssh;
+#echo "|-- Done!";
 
 # installs my favourite font! :)
-gimme atool;
 
-COUSINE_URL='http://www.google.com/fonts/download?kit=M2CeFoF86bDfqp_rSFi-T6CWcynf_cDxXwCLxiixG1c';
-wget -nv -O Cousine.zip $COUSINE_URL;
-aunpack -q Cousine.zip;
-mkdir -p ~/.fonts;
-cp Cousine/*.ttf ~/.fonts;
-sudo fc-cache -f;
-rm Cousine.zip;
-rm -rf Cousine/;
+COUSINE_URL="$URL_ROOT/Cousine.zip";
+#wget -nv -O Cousine.zip $COUSINE_URL;
+#aunpack -q Cousine.zip;
+#mkdir -p ~/.fonts;
+#cp Cousine/*.ttf ~/.fonts;
+#sudo fc-cache -f;
+#rm Cousine.zip;
+#rm -rf Cousine/;
