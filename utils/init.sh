8543285cbb53d6e75b707dfc44b3ba83d70b909b
Oscar Bezi <oscar@bezi.io>
2014-06-08 00:10:14 -0400

    Colourised output.

--- utils/init.sh
+++ utils/init.sh
@@ -6,9 +6,14 @@
 # @since 6 June 2014
 #===============================================================================
 
+# Because the alias file might not have the colours
+BGreen='\[\e[1;92m\]'      # Green
+BYellow='\[\e[1;93m\]'     # Yellow
+BBlue='\[\e[1;94m\]'       # Blue
+
 # download files and stick them in the right places, then source the ~/.bashrc
 # note that I didn't get a .bash_locations: this is a per-computer file
-echo "Downloading files. . .";
+echo "$BBlueDownloading files. . .";
 
 URL_ROOT='https://raw.githubusercontent.com/bezi/bezi.github.io/master/utils';
 # .bashrc
@@ -20,29 +25,44 @@ URL_ROOT='https://raw.githubusercontent.com/bezi/bezi.github.io/master/utils';
 # .ssh/config
 #wget -nv -O ~/.ssh/config $URL_ROOT/.ssh/config;
 
-echo "|-- Done!";
+echo "$BGreen|-- Done!";
 
 shopt -s expand_aliases;
 source ~/.bashrc;
 
 # programs I use a lot. gimme is an alias to the installer for the OS
-#gimme vim;
-#gimme git;
-# gimme sshpass;
-#gimme xclip;
-#gimme atool;
-
+echo "$BBlueInstalling programs. . .";
+gimme vim;
+gimme git;
+gimme sshpass;
+gimme xclip;
+gimme atool;
+gimme firefox;
+echo "$BGreen|-- Done!";
 
 # git configuration
-echo "Configuring git. . .";
-#git config --global user.name "Oscar Bezi";
-#git config --global user.email "oscar@bezi.io";
-#git config --global color.ui true;
-#git config --global push.default simple;
-echo "|-- Done!";
+echo "$BBlueConfiguring git. . .";
+git config --global user.name "Oscar Bezi";
+git config --global user.email "oscar@bezi.io";
+git config --global color.ui true;
+git config --global push.default simple;
+echo "$BGreen|-- Done!";
+
+# installs my favourite font! :)
+
+echo "$BBlueInstalling Cousine. . .";
+COUSINE_URL="$URL_ROOT/Cousine.zip";
+wget -nv -O Cousine.zip $COUSINE_URL;
+aunpack -q Cousine.zip;
+mkdir -p ~/.fonts;
+cp Cousine/*.ttf ~/.fonts;
+sudo fc-cache -f;
+rm Cousine.zip;
+rm -rf Cousine/;
+echo "$BGreen|-- Done!";
 
 # generate SSH keys and propagate them to the various things in ~/.ssh/config
-echo "Distributing ssh keys. . .";
+echo "$BBlueDistributing ssh keys. . .";
 yes y | ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa;
 
 
@@ -82,25 +102,15 @@ for i in `cat ~/.ssh/config | grep "^host " | awk '{print $2}'`; do
     else 
         echo "FAILURE: STATUS $STATUS";
     fi
+    rm $i.password
     
 done
 
 # add ssh keys to github: cannae automate :(
 # from github; copies the key to my clipboard so I can go paste it into ffox
-#xclip -sel clip < ~/.ssh/id_rsa.pub
+xclip -sel clip < ~/.ssh/id_rsa.pub
 
-#echo "|-- SSH key copied to clipboard.  Opening firefox to add to github. . ." ;
-#gimme firefox;
-#firefox https://github.com/settings/ssh;
-#echo "|-- Done!";
-
-# installs my favourite font! :)
-
-COUSINE_URL="$URL_ROOT/Cousine.zip";
-#wget -nv -O Cousine.zip $COUSINE_URL;
-#aunpack -q Cousine.zip;
-#mkdir -p ~/.fonts;
-#cp Cousine/*.ttf ~/.fonts;
-#sudo fc-cache -f;
-#rm Cousine.zip;
-#rm -rf Cousine/;
+echo "$BCyan|-- SSH key copied to clipboard.  Opening firefox to add to github. . ." ;
+firefox https://github.com/settings/ssh;
+echo "$BGreen|-- Done!";
+echo "$BGreenSetup complete!$No_color";
