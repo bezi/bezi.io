24e3fa4f3b3eb831154dbc4bfd65f7e658c6d97a
Oscar Bezi <oscar@bezi.io>
2014-06-10 13:57:34 -0400

    Correct color codes.

--- utils/init.sh
+++ utils/init.sh
@@ -7,50 +7,50 @@
 #===============================================================================
 
 # Because the alias file might not have the colours
-BGreen='\[\e[1;92m\]'      # Green
-BYellow='\[\e[1;93m\]'     # Yellow
-BBlue='\[\e[1;94m\]'       # Blue
+GREEN='\e[1;92m'      # Green
+YELLOW='\e[1;93m'     # Yellow
+BLUE='\e[1;94m'       # Blue
 
 # download files and stick them in the right places, then source the ~/.bashrc
 # note that I didn't get a .bash_locations: this is a per-computer file
-echo -e "$BBlueDownloading files. . .";
+echo -e "${BLUE}Downloading files. . .";
 
 URL_ROOT='https://raw.githubusercontent.com/bezi/bezi.github.io/master/utils';
 # .bashrc
-#wget -nv -O ~/.bashrc $URL_ROOT/.bashrc;
+wget -nv -O ~/.bashrc $URL_ROOT/.bashrc;
 # .bash_aliases
-#wget -nv -O ~/.bash_aliases $URL_ROOT/.bash_aliases;
+wget -nv -O ~/.bash_aliases $URL_ROOT/.bash_aliases;
 # .vimrc
-#wget -nv -O ~/.vimrc $URL_ROOT/.vimrc;
+wget -nv -O ~/.vimrc $URL_ROOT/.vimrc;
 # .ssh/config
-#wget -nv -O ~/.ssh/config $URL_ROOT/.ssh/config;
+wget -nv -O ~/.ssh/config $URL_ROOT/.ssh/config;
 
-echo -e "$BGreen|-- Done!";
+echo -e "${GREEN}|-- Done!";
 
 shopt -s expand_aliases;
 source ~/.bashrc;
 
 # programs I use a lot. gimme is an alias to the installer for the OS
-echo -e "$BBlueInstalling programs. . .";
+echo -e "${BLUE}Installing programs. . .";
 gimme vim;
 gimme git;
 gimme sshpass;
 gimme xclip;
 gimme atool;
 gimme firefox;
-echo -e "$BGreen|-- Done!";
+echo -e "${GREEN}|-- Done!";
 
 # git configuration
-echo -e "$BBlueConfiguring git. . .";
+echo -e "${BLUE}Configuring git. . .";
 git config --global user.name "Oscar Bezi";
 git config --global user.email "oscar@bezi.io";
 git config --global color.ui true;
 git config --global push.default simple;
-echo -e "$BGreen|-- Done!";
+echo -e "${GREEN}|-- Done!";
 
 # installs my favourite font! :)
 
-echo -e "$BBlueInstalling Cousine. . .";
+echo -e "${BLUE}Installing Cousine. . .";
 COUSINE_URL="$URL_ROOT/Cousine.zip";
 wget -nv -O Cousine.zip $COUSINE_URL;
 aunpack -q Cousine.zip;
@@ -59,10 +59,10 @@ cp Cousine/*.ttf ~/.fonts;
 sudo fc-cache -f;
 rm Cousine.zip;
 rm -rf Cousine/;
-echo -e "$BGreen|-- Done!";
+echo -e "${GREEN}|-- Done!";
 
 # generate SSH keys and propagate them to the various things in ~/.ssh/config
-echo -e "$BBlueDistributing ssh keys. . .";
+echo -e "${BLUE}Distributing ssh keys. . .";
 yes y | ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa;
 
 
@@ -110,7 +110,7 @@ done
 # from github; copies the key to my clipboard so I can go paste it into ffox
 xclip -sel clip < ~/.ssh/id_rsa.pub
 
-echo -e "$BCyan|-- SSH key copied to clipboard.  Opening firefox to add to github. . ." ;
+echo -e "${YELLOW}|-- SSH key copied to clipboard.  Opening firefox to add to github. . ." ;
 firefox https://github.com/settings/ssh;
-echo -e "$BGreen|-- Done!";
-echo -e "$BGreenSetup complete!$No_color";
+echo -e "${GREEN}|-- Done!";
+echo -e "${GREEN}Setup complete!$No_color";
